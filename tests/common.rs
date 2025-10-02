10fdac447d26ad2763c37772b3ca6dc6c90eba76
Kesomannen <bobbo.nidor@gmail.com>
2025-10-02 19:37:29 +0200

    remove util func

--- tests/common.rs
+++ tests/common.rs
@@ -1,17 +1,8 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use loadsmith::AnyZipArchive;
-use tempfile::TempDir;
 
 pub fn open_zip(name: &str) -> AnyZipArchive {
     let path: PathBuf = format!("{}/tests/data/{name}.zip", env!("CARGO_MANIFEST_DIR")).into();
     loadsmith::open_zip(path).expect("error opening test zip archive")
-}
-
-pub fn assert_exists(tempdir: &TempDir, path: impl AsRef<Path>) {
-    assert!(
-        tempdir.path().join(&path).exists(),
-        "{} did not exist!",
-        path.as_ref().display()
-    );
 }
